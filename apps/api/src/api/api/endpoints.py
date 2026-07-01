eaa592665810afd651140256ee283c8bdc0f36e0
aurimasgriciunas <griciunas.aurimas@gmail.com>
2026-07-01 14:15:06 +0300

    Release Week 2

--- apps/api/src/api/api/endpoints.py
+++ apps/api/src/api/api/endpoints.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Request
-from api.api.models import RAGRequest, RAGResponse
-from api.agents.retrieval_generation import rag_pipeline
-from qdrant_client import QdrantClient
+from api.api.models import RAGRequest, RAGResponse, RAGUsedContext
+from api.agents.retrieval_generation import rag_pipeline_wraper
 import logging
 
 logging.basicConfig(
@@ -12,17 +11,18 @@ logger = logging.getLogger(__name__)
 
 rag_router = APIRouter()
 
-qdrant_client = QdrantClient(url="http://qdrant:6333")
-
 @rag_router.post("/")
 def chat(
     request: Request,
     payload: RAGRequest
 ) -> RAGResponse:
 
-    result = rag_pipeline(payload.query, qdrant_client)
+    result = rag_pipeline_wraper(payload.query)
 
-    return RAGResponse(answer=result["answer"])
+    return RAGResponse(
+        answer=result["answer"], 
+        used_context=[RAGUsedContext(**item) for item in result["used_context"]]
+    )
 
 api_router = APIRouter()
 api_router.include_router(rag_router, prefix="/rag", tags=["rag"])
